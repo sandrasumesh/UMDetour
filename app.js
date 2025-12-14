cb24401d63a88c96a5958188868ef3d785e9ad22
Sandra Sumesh <sandrasumesh@sandras-air-10.wireless.dyn.umd.edu>
2025-12-14 04:10:38 -0500

    initial

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use('/new', newRoutes);
 const mapRoutes = require('./routes/mapRoutes');
 app.use('/', mapRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+
 });
 
